afa086ee701a4a61f3abf263e6ea5762a95835fa
lowrezdragon@gmail.com <lowrezdragon@gmail.com>
2024-12-30 13:39:39 -0600

    Working on checks for to_html

--- public/src/htmlnode.py
+++ public/src/htmlnode.py
@@ -19,12 +19,19 @@ class HTMLNode:
 		return (f" tag=", self.tag, " value=", self.value," children=", self.children,
 		" props=",self.props
 		)
-class LeafNode:
+class LeafNode(HTMLNode):
 	def __init__(self, tag=None, value=None, props=None):
-		super().__init__(self, tag, value, props)
+		super().__init__(tag, value, props,children=None)
 
 	def to_html(self):
-		if self.value:
-			if not self.tag:
+		if not isinstance(self.value, None):
+			if not self.tag or self.tag == "":
 				return self.value
+			if isinstance(self.tag, str):
+				return f"<{self.tag}>{self.value}</{self.tag}>"
+			if isinstance(self.tag, dict):
+				
+				pass
+			
+		raise ValueError("All leaf nodes need a value")
 			
