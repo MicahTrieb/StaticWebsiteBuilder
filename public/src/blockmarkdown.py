351009ca0617fe640719a9d3ca68ea533a0321b9
lowrezdragon <lowrezdragon@gmail.com>
2025-01-07 23:58:50 -0600

    Working on regex, got code block done

--- public/src/blockmarkdown.py
+++ public/src/blockmarkdown.py
@@ -6,8 +6,13 @@ def markdown_to_blocks(inputString):
 
 def block_to_blocktype(inputBlock):
     headerRegex = re.findall(r"^#+(?= )", inputBlock)
-    #print(headerRegex)
     if headerRegex:
         return "header"
+    codeBlock1 = re.findall(r"^`{3}", inputBlock)
+    codeBlock2 = re.findall(r"`{3}(?=$)", inputBlock)
+    if(codeBlock1 and codeBlock2):
+        return "code"
+    
+    if():
     else:
         return "normal"
