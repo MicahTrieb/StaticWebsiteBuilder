bf3b4ae7bd493882031f74f5d3923e06936119bf
lowrezdragon@gmail.com <lowrezdragon@gmail.com>
2025-01-16 14:16:47 -0600

    Trying to work out why it wont find the directory

--- src/directorycopy.py
+++ src/directorycopy.py
@@ -8,20 +8,24 @@ def directory_copy(movingDirectory, targetDirectory):
         possibleNo = ["No", "no", "N", "n", "NO"]
         possibleAnswers = possibleNo + possibleYes
         while createAnswer not in possibleAnswers:
-            createAnswer = input(f"Directory not found, would you like to create directory {targetDirectory}?")
+            createAnswer = input(f"Directory not found, would you like to create directory {targetDirectory}?\n")
         if createAnswer in possibleNo:
             raise Exception("Target directory does not exist")
         elif createAnswer in possibleYes:
             os.mkdir(targetDirectory)
-    deleteDirectory = input(f"Warning, this will remove the target directory: {targetDirectory}, are you sure?")
-    if deleteDirectory in possibleYes:
-        shutil.rmtree(targetDirectory)
-        listToBeCopied = os.listdir(movingDirectory)
-
+            copying_function(movingDirectory, targetDirectory)
     else:
-        raise Exception("Directory will not be deleted")
+        deleteAnswer = input(f"Warning, this will delete {targetDirectory}, are you sure? \n")
+        if deleteAnswer in possibleYes:
+            shutil.rmtree(targetDirectory)
+            os.mkdir(targetDirectory)
+            copying_function(movingDirectory, targetDirectory)
+
+        else:
+            raise Exception("Directory will not be deleted")
     
 def copying_function(movingDirectory, targetDirectory):
     listToBeCopied = os.listdir(movingDirectory)
     for currentDirectory in listToBeCopied:
+        print(currentDirectory)
         shutil.copy(currentDirectory, targetDirectory)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
 from textnode import *
 from directorycopy import directory_copy, copying_function
 def main():
 	print("Hello world! Executing main.py!")
-	directory_copy
+	directory_copy("/home/mici/gitHub/MicahsProjects/staticWebsite/static", "/home/mici/gitHub/MicahsProjects/staticWebsite/public/testDirectory")
 
 main()
 	
